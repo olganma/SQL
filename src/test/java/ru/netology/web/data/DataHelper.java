d790abd847c983d64aec44620976d965650cf5e9
Maevskaya Olga <olganma@gmail.com>
2023-03-14 23:44:04 +0300

    Positive test created

---
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -0,0 +1,23 @@
+package ru.netology.web.data;
+import lombok.Value;
+
+public class DataHelper {
+
+    private DataHelper() {
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+}
---
+++ src/test/java/ru/netology/web/test/PositiveTest.java
@@ -0,0 +1,22 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.data.SQLHelper;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.*;
+import static ru.netology.web.data.SQLHelper.cleanDatabase;
+
+public class PositiveTest {
+
+    @Test
+    void shouldLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(SQLHelper.getVerificationCode());
+    }
+}
